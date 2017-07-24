1ede709db5281aa167e886a201c8bf1e2b28b2ce
Nadezhda Kutsenko <nadikutsenko@gmail.com>
2017-07-25 00:15:08 +0200

    Add carousel

--- src/components/2carousel/Carousel.js
+++ src/components/2carousel/Carousel.js
@@ -3,23 +3,48 @@ import './carouselStyle.css';
 import {Motion, spring} from 'react-motion';
 import bookInfo from '../../books.json';
 import BookCard from './BookCard/BookCard';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 var Carousel = React.createClass({
   getCategoryBook() {
+    const colors = ['background-color:#F8BBD0', 'background-color:#69F0AE', 'background-color:#E1BEE7', 'background-color:#90CAF9', 'background-color:#CCFF90']
     let carouselBooks = [];
     for (let i=0; i < bookInfo.store.length; i++) {
       carouselBooks.push(
-        <BookCard key={i} bookInfo={bookInfo.store[i].books[0]}></BookCard>
+        <div key={i} style='height:450px;'><BookCard bookInfo={bookInfo.store[i].books[0]}></BookCard></div>
       );
     };
     console.log(carouselBooks);
     return carouselBooks;
   },
-  render: function() {
+  // render: function() {
+  //   return (
+  //     <div className='Carousel'>
+  //       
+  //     </div>
+  //   );
+  // }
+  render: function () {
+    var settings = {
+      className: 'center',
+      centerMode: true,
+      infinite: true,
+      centerPadding: '30px',
+      slidesToShow: 3,
+      speed: 500
+      
+    };
     return (
       <div className='Carousel'>
-        
+        {/* <button class='slick-arrow slick-prev'>prev</button> */}
+        <Slider {...settings}>
+          {this.getCategoryBook()}
+        </Slider>
+        {/* <button class='slick-arrow slick-next'>next</button> */}
       </div>
+      
     );
   }
 });
